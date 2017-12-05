cb5ffb6c3f16c0174a85c859b4731243bd7f9619
Benoit Boccard <benoit-externe.boccard@edf.fr>
2017-12-05 15:28:22 +0100

    slurm-wckeys-setup: Update header

--- job_submit/slurm_wckeys_setup.sh
+++ job_submit/slurm_wckeys_setup.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
-# slurm_wckeys_setup.sh is part of Calibre
-#
-# Copyright (C) 2015 EDF SA
-# Contact:
-#       CCN - HPC <dsp-cspit-ccn-hpc@edf.fr>
-#       1, Avenue du General de Gaulle
-#       92140 Clamart
-#
-#Author: Antonio J. Russo <antonio-externe.russo@edf.fr>
-#This program is free software; you can redistribute in and/or
-#modify it under the terms of the GNU General Public License,
-#version 2, as published by the Free Software Foundation.
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
-#On Calibre systems, the complete text of the GNU General
-#Public License can be found in `/usr/share/common-licenses/GPL'.
-###################################################################
+# -*- coding: utf-8 -*-
+##############################################################################
+#  Copyright (C) 2015-2017 EDF SA                                            #
+#                                                                            #
+#  Author: CCN HPC <dsp
+#                                                                            #
+#  This file is part of slurm-llnl-misc-plugins.                             #
+#                                                                            #
+#  This software is governed by the CeCILL-C license under French law and    #
+#  abiding by the rules of distribution of free software. You can use,       #
+#  modify and/ or redistribute the software under the terms of the CeCILL-C  #
+#  license as circulated by CEA, CNRS and INRIA at the following URL         #
+#  "http://www.cecill.info".                                                 #
+#                                                                            #
+#  As a counterpart to the access to the source code and rights to copy,     #
+#  modify and redistribute granted by the license, users are provided only   #
+#  with a limited warranty and the software's author, the holder of the      #
+#  economic rights, and the successive licensors have only limited           #
+#  liability.                                                                #
+#                                                                            #
+#  In this respect, the user's attention is drawn to the risks associated    #
+#  with loading, using, modifying and/or developing or reproducing the       #
+#  software by the user in light of its specific status of free software,    #
+#  that may mean that it is complicated to manipulate, and that also         #
+#  therefore means that it is reserved for developers and experienced        #
+#  professionals having in-depth computer knowledge. Users are therefore     #
+#  encouraged to load and test the software's suitability as regards their   #
+#  requirements in conditions enabling the security of their systems and/or  #
+#  data to be ensured and, more generally, to use and operate it in the      #
+#  same conditions as regards security.                                      #
+#                                                                            #
+#  The fact that you are presently reading this means that you have had      #
+#  knowledge of the CeCILL-C license and that you accept its terms.          #
+#                                                                            #
+##############################################################################
 
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 CMD=$(basename $0)

e11475f1bda1008778bcacaf4d62c8ed817312be
Thomas HAMEL <thomas-t.hamel@edf.fr>
2020-12-10 19:37:52 +0100

    add a task prolog to initialize kerberos/lustre

---
+++ TaskProlog.d/kerberos_lustre.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+##### Setting up lustre kerberos context
+## Set the canonical credential cache path, that is where
+## lustre (lgss) will search for the credentials
+user_numerical_id=$(id -u)
+canonical_ccache="/tmp/krb5cc_${user_numerical_id}"
+
+if [[ ${user_numerical_id} == "x0" ]]
+then
+  # Root has root squash or not, but he does not need this to
+  # access its home
+  exit 0
+fi
+
+# Find the first lustre mount point
+lustre_mountpoint="$(LANG=C mount -t lustre | head -n1 |sed -n "s/.* on \(.*\) type lustre .*/\1/;T;p")"
+if [[ -z ${lustre_mountpoint} ]]
+then
+  # No lustre mountpoint, this is pointless
+  exit 0
+fi
+
+if [[ -z "$KRB5CCNAME" ]]
+then
+  # That's not good, we can't do much, if there is already a valid
+  # credential cache in the canonical path that might work.
+  echo "No KRB5CCNAME defined" >&2
+  klist >&2
+else
+  if [[ $KRB5CCNAME != "FILE:${canonical_ccache}" ]]
+  then
+    # Putting the cache in the canoncial place, cp is a crude tool
+    # to do this, delegating a credential would be better
+    cp $(echo $KRB5CCNAME | cut -d ":" -f 2) ${canonical_ccache}
+  fi
+fi
+
+# And now the confusing part
+
+# Trying to access lustre, this triggers a new key
+#  * If an access was already attempted, the new key will be properly
+#    initialized but this access will still fail because of the bad
+#    key that will be removed next
+#  * If this is the first access, it should succeed
+ls ${lustre_mountpoint}  > /dev/null 2>&1
+
+#
+# The keyring has an unitialized key, reaping it will force the
+# good key to take its place.
+keyctl reap >/dev/null 2>&1
+
